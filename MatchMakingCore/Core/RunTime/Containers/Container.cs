eb314ef06de7446ff14be020152ec055cf9073a4
Tan Li <tanli1622@gmail.com>
2020-11-07 09:56:36 +0800

    [DEV] deserialize player data.

--- MatchMakingCore/Core/RunTime/Containers/Container.cs
+++ MatchMakingCore/Core/RunTime/Containers/Container.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace MatchMakingCore
 {
@@ -13,9 +14,20 @@ namespace MatchMakingCore
 
         public void Test()
         {
-            Console.WriteLine(File.Exists(PLAYER_DATA_PATH));
 
+            Console.WriteLine(File.Exists(PLAYER_DATA_PATH));
             Console.WriteLine(Directory.GetCurrentDirectory());
+            using (StreamReader stream = new StreamReader(PLAYER_DATA_PATH))
+            {
+                string str = stream.ReadToEnd();
+                Console.WriteLine(str);
+
+                var dataList = JsonConvert.DeserializeObject<PlayerData[]>(str);
+                for(int i = 0; i < dataList.Length; ++i)
+                {
+                    Console.WriteLine(dataList[i].ToString());
+                }
+            }            
         }
     }
 }
---
+++ MatchMakingCore/Core/RunTime/Containers/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MatchMakingCore
+{
+    public class PlayerData
+    {
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("wins")] public string Wins;
+        [JsonProperty("loses")] public string Loses;
+
+        public override string ToString()
+        {
+            return $"Name {Name} Wins {Wins} Loses {Loses}";
+        }
+    }
+}
